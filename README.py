dd8c7ec9973cfe487567266f55ee7000a18cf4c5
Jayko14 <43162450+Jayko14@users.noreply.github.com>
2018-09-13 16:27:11 +1000

    Update README.py

--- README.py
+++ README.py
@@ -1,12 +1,18 @@
-# Practice
-a = range(1,e.randint(1,100))
-b = range(1,f.randint(1,100))
+import random
+while True:
+    x = ['rock','paper','scissors']
+    P1 = input('Please choose from rock, paper or scissors ')
+    P2 = random.choice(x)
+    print('Computer chose ' + P2)
+    if P1 == P2:
+        print ("It's a tie")
+    elif P1 == 'rock' and P2 == 'scissors':
+        print ("You win!")
+    elif P1 == 'paper' and P2 == 'rock':
+        print ("You win!")
+    elif P1 == 'scissors' and P2 == 'paper':
+        print ("You win!")
+    else:
+        print ("You lose!")
+        break        
 
-c = []
-
-for i in b:
-    if i in a:
-        if i not in c:
-            c.append(i)
-
-print(c)
