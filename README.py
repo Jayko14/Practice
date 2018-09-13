b60addd71cbb5b4b06d5bae6d1e9f714ae69bd32
Jayko14 <43162450+Jayko14@users.noreply.github.com>
2018-09-13 17:23:28 +1000

    Update README.py

--- README.py
+++ README.py
@@ -1,21 +1,33 @@
+print("Let's play Rock, Paper, Scissors")
+P1_score = 0
+Comp_score = 0
+
 import random
-while True:
+y = 'yes'
+while y =='yes':
     x = ['rock','paper','scissors']
     P1 = input('Please choose from rock, paper or scissors ')
     P2 = random.choice(x)
     print('Computer chose ' + P2)
-    if P1 != ('rock' or 'paper'or 'scissors'):
-        print ("Invalid entry")
     if P1 == P2:
-        print ("It's a tie")
-    elif P1 == 'rock' and P2 == 'scissors':
-        print ("You win!")
-    elif P1 == 'paper' and P2 == 'rock':
-        print ("You win!")
-    elif P1 == 'scissors' and P2 == 'paper':
-        print ("You win!")
+         print ("It's a tie")
+         continue
+    elif P1 == 'rock' and P2 == 'scissors'or P1 == 'paper' and P2 == 'rock' or P1 == 'scissors' and P2 == 'paper':
+         print ("You win!")
+         P1_score += 1
+    elif P1 == 'paper' and P2 == 'scissors' or P1 == 'scissors' and P2 == 'rock' or P1 == 'rock' and P2 == 'paper':
+         print ("You lose!")
+         Comp_score += 1
     else:
-        print ("You lose!")
-        break        
+        print ("Invalid entry please try again")
+        
+    print("Player 1 Score:" + str(P1_score))
+    print("Computer Score:" + str(Comp_score))
+    y = input('Would you like to play again? yes/no ')
+    if y == 'yes':
+        continue
+    else:
+        break
+print('Thanks for playing')
 
-
+    
