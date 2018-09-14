7b24760c7bfa4e28552457e9bd2acae3398068fb
Jayko14 <43162450+Jayko14@users.noreply.github.com>
2018-09-14 17:15:07 +1000

    Update README.py

--- README.py
+++ README.py
@@ -1,27 +1,8 @@
-print('Welcome to the guessing game')
-print('Type quit to leave at any time')
 import random
-x = random.randint (1,10)
-guess=0
-y=0
-while y != x:
-    y = input('Please pick a number between 0 and 10 ')
-    if y == 'quit':
-        print('Thanks for playing')
-        break
-    y = int(y)
-    if y<= 0 or y>= 10:
-        print('Invalid entry')
-    elif y > x:
-        print('Your guess is too high!')
-        guess += 1
-    elif y < x:
-        print('Your guess is too low!')
-        guess += 1
-    else:
-        print('Your guess is correct!')
-        guess += 1
-        print('Number of guesses:' + str(guess))
-        break
-       
-    
+x= random.sample(range(1,50),20)
+y= random.sample(range(1,100),20)
+z=[i for i in x if i in y]
+
+print(x)
+print(y)
+print(z)
