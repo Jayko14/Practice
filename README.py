789663a63aa90ea39ba52508c5d11056909fc4fd
Jayko14 <43162450+Jayko14@users.noreply.github.com>
2018-09-14 12:40:14 +1000

    Update README.py

--- README.py
+++ README.py
@@ -1,9 +1,9 @@
 print("Let's play Rock, Paper, Scissors")
-P1_score = 0
-Comp_score = 0
 
 import random
 y = 'yes'
+P1_score = 0
+Comp_score = 0
 while y =='yes':
     x = ['rock','paper','scissors']
     P1 = input('Please choose from rock, paper or scissors ')
@@ -20,14 +20,26 @@ while y =='yes':
          Comp_score += 1
     else:
         print ("Invalid entry please try again")
-        
     print("Player 1 Score:" + str(P1_score))
     print("Computer Score:" + str(Comp_score))
-    y = input('Would you like to play again? yes/no ')
-    if y == 'yes':
-        continue
-    else:
-        break
+    if P1_score == 5:
+        print ('You beat Computer')
+        y = input('Would you like to play again? yes/no ')
+        if y == 'yes':
+            P1_score=0
+            Comp_score=0
+            continue
+        else:
+            break
+    if Comp_score == 5:
+        print ('Computer beat you')
+        y = input('Would you like to play again? yes/no ')
+        if y == 'yes':
+            P1_score=0
+            Comp_score=0            
+            continue
+        else:
+            break
 print('Thanks for playing')
 
     
