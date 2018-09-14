0150b75aa67b5d410fc443a8db90aa955367b46f
Jayko14 <43162450+Jayko14@users.noreply.github.com>
2018-09-14 16:04:08 +1000

    Update README.py

--- README.py
+++ README.py
@@ -1,45 +1,27 @@
-print("Let's play Rock, Paper, Scissors")
-
+print('Welcome to the guessing game')
+print('Type quit to leave at any time')
 import random
-y = 'yes'
-P1_score = 0
-Comp_score = 0
-while y =='yes':
-    x = ['rock','paper','scissors']
-    P1 = input('Please choose from rock, paper or scissors ')
-    P2 = random.choice(x)
-    print('Computer chose ' + P2)
-    if P1 == P2:
-         print ("It's a tie")
-         continue
-    elif P1 == 'rock' and P2 == 'scissors'or P1 == 'paper' and P2 == 'rock' or P1 == 'scissors' and P2 == 'paper':
-         print ("You win!")
-         P1_score += 1
-    elif P1 == 'paper' and P2 == 'scissors' or P1 == 'scissors' and P2 == 'rock' or P1 == 'rock' and P2 == 'paper':
-         print ("You lose!")
-         Comp_score += 1
+x = random.randint (1,10)
+guess=0
+y=0
+while y != x:
+    y = input('Please pick a number between 0 and 10 ')
+    if y == 'quit':
+        print('Thanks for playing')
+        break
+    y = int(y)
+    if y<= 0 or y>= 10:
+        print('Invalid entry')
+    elif y > x:
+        print('Your guess is too high!')
+        guess += 1
+    elif y < x:
+        print('Your guess is too low!')
+        guess += 1
     else:
-        print ("Invalid entry please try again")
-    print("Player 1 Score:" + str(P1_score))
-    print("Computer Score:" + str(Comp_score))
-    if P1_score == 5:
-        print ('You beat Computer')
-        y = input('Would you like to play again? yes/no ')
-        if y == 'yes':
-            P1_score=0
-            Comp_score=0
-            continue
-        else:
-            break
-    if Comp_score == 5:
-        print ('Computer beat you')
-        y = input('Would you like to play again? yes/no ')
-        if y == 'yes':
-            P1_score=0
-            Comp_score=0            
-            continue
-        else:
-            break
-print('Thanks for playing')
-
+        print('Your guess is correct!')
+        guess += 1
+        print('Number of guesses:' + str(guess))
+        break
+       
     
