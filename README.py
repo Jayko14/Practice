7974ad1e33777419d2ba00922f67c843ff95bd40
Jayko14 <43162450+Jayko14@users.noreply.github.com>
2018-09-25 22:15:35 +1000

    Update README.py

--- README.py
+++ README.py
@@ -1,7 +1,5 @@
-x = int(input(' Please choose how many fibonacci numbers you want to display '))
-
-fib = [1,1]
-for i in range (2, x):
-    fib.append(fib[i-1] + fib[i-2])
-
-print (fib)
+while True:
+    x = input('Please type out a sentence ')
+    y = x.split(" ")
+    z = " ".join(reversed(y))
+    print(z)
